3f09a07b69017069ba2df0071e444b67b3c8027a
Vardhan <ksaivardhan9@gmail.com>
2026-06-22 19:05:04 +0530

    first event tasks done

--- Intermediate Javascript/Day - 14/Events.js
+++ Intermediate Javascript/Day - 14/Events.js
@@ -47,4 +47,17 @@ sel.addEventListener("change", function(details){
     select.textContent = "Device Selected";
 });
 
+// Task we click any key names comes on screen
+
+let h1 = document.querySelector("h1")
+window.addEventListener("keydown", function(dets){
+    if (dets.key === " "){
+        h1.textContent = "SPC"
+    } else {
+        h1.textContent = dets.key;
+    }
+ 
+})
+
+
 
