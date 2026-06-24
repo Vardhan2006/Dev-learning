c2271b8a5a98874a75a585ae42e53b52f1f6819a
Vardhan <ksaivardhan9@gmail.com>
2026-06-24 12:49:01 +0530

    Common events: submit completed build profile card builder

--- Intermediate Javascript/Day - 15/Events_2.js
+++ Intermediate Javascript/Day - 15/Events_2.js
@@ -1,37 +1,17 @@
-// submit event triggers when user clicks submit button
-// by default form submission reloads the page
-// preventDefault() stops the default browser action
-
 let form = document.querySelector("#form");
-let inputs = document.querySelectorAll("#input");
-let main = document.querySelector("#main");
 
-form.addEventListener("submit", function (dets) {
-    dets.preventDefault();
+let nameInput = document.querySelectorAll("#input input")[0];
+let ageInput = document.querySelectorAll("#input input")[1];
+let emailInput = document.querySelectorAll("#input input")[2];
 
-    let card = document.createElement("div");
-    card.classList.add("card");
+let cardName = document.querySelector(".card h3");
+let cardRole = document.querySelector(".card h5");
+let cardDesc = document.querySelector(".card p");
 
-    let profile = document.createElement("div")
-    profile.classList.add("profile");
+form.addEventListener("submit", function (e) {
+    e.preventDefault();
 
-    let img = document.createElement("img");
-    img.setAttribute("src", "https://plus.unsplash.com/premium_photo-1688572454849-4348982edf7d?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDk4fHRvd0paRnNrcEdnfHxlbnwwfHx8fHw%3D");
-
-    let h3 = document.createElement("h3");
-    h3.textContent = "Lady";
-    let h5 = document.createElement("h5");
-    h5.textContent = "BOOOOOOOOM"
-    let p = document.createElement("p");
-    p.textContent = "I am a product designer who lover desiging products";
-
-    profile.appendChild(img);
-    card.appendChild(profile);
-
-    card.appendChild(h3);
-    card.appendChild(h5);
-    card.appendChild(p);
-
-    main.appendChild(card);
-
+    cardName.textContent = nameInput.value;
+    cardRole.textContent = ageInput.value;
+    cardDesc.textContent = emailInput.value;
 });
