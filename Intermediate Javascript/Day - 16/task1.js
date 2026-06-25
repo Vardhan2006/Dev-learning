d96f38474df5f53178a15f4c3c17196c5ccbee95
Vardhan <ksaivardhan9@gmail.com>
2026-06-25 18:45:25 +0530

    Completed Events and Event Handling

--- Intermediate Javascript/Day - 16/task1.js
+++ Intermediate Javascript/Day - 16/task1.js
@@ -2,5 +2,27 @@ let inp = document.querySelector("input");
 let span = document.querySelector("span");
 
 inp.addEventListener("input", function(){
-    span.textContent = inp.value.length;
+    
+    let left = 20 - inp.value.length;
+    span.textContent = left;
+
+    if (left < 0) {
+        span.style.color = "red";
+    } else {
+        span.style.color = "black";
+    }
 });
+
+
+/* 
+
+common confusions
+
+Events and event handling:
+
+1. events vs listener
+
+click, dbl click is event and what happens after 
+that what the functon does is event listner
+
+*/
