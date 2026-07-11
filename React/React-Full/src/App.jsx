33642f1ccdb4c567e7b405e74114ba6fe55215dc
Vardhan <ksaivardhan9@gmail.com>
2026-07-11 17:45:30 +0530

    Completed Props mini project

--- React/React-Full/src/App.jsx
+++ React/React-Full/src/App.jsx
@@ -1,19 +1,93 @@
 import Props_Project from "./Day - 2/Props_Project";
 
 function App() {
-  
+
+  const jobs = [
+    {
+      id: 1,
+      company: "Amazon",
+      logo: "https://logo.clearbit.com/amazon.com",
+      posted: "5 days ago",
+      role: "Senior Backend Engineer",
+      type: "Full Time",
+      level: "Senior Level",
+      salary: "$45k/Month",
+      location: "Seattle, USA"
+    },
+    {
+      id: 2,
+      company: "Google",
+      logo: "https://logo.clearbit.com/google.com",
+      posted: "2 days ago",
+      role: "Frontend Developer",
+      type: "Full Time",
+      level: "Mid Level",
+      salary: "$40k/Month",
+      location: "California, USA"
+    },
+    {
+      id: 3,
+      company: "Microsoft",
+      logo: "https://logo.clearbit.com/microsoft.com",
+      posted: "1 week ago",
+      role: "Software Engineer",
+      type: "Full Time",
+      level: "Junior Level",
+      salary: "$35k/Month",
+      location: "Redmond, USA"
+    },
+    {
+      id: 4,
+      company: "Netflix",
+      logo: "https://logo.clearbit.com/netflix.com",
+      posted: "3 days ago",
+      role: "React Developer",
+      type: "Remote",
+      level: "Mid Level",
+      salary: "$38k/Month",
+      location: "Los Angeles, USA"
+    },
+    {
+      id: 5,
+      company: "Spotify",
+      logo: "https://logo.clearbit.com/spotify.com",
+      posted: "Today",
+      role: "Node.js Developer",
+      type: "Remote",
+      level: "Senior Level",
+      salary: "$42k/Month",
+      location: "Stockholm, Sweden"
+    },
+    
+    {
+      id: 6,
+      company: "Adobe",
+      logo: "https://logo.clearbit.com/adobe.com",
+      posted: "6 days ago",
+      role: "UI Engineer",
+      type: "Hybrid",
+      level: "Mid Level",
+      salary: "$36k/Month",
+      location: "San Jose, USA"
+    },
+    
+  ];
+
+
   return (
     <div className="parent">
 
-      <Props_Project/>
-      <Props_Project/>
+      <div className="parent">
+        {jobs.map((job) => (
+          <Props_Project key={job.id} job={job} />
+        ))}
+      </div>
 
       {/* <Props user="BOB" />
       <Props user="ROB" />
       <Props user="HOB" />
       <Props user="HOP" /> */}
     </div>
-    
 
   )
 }
--- React/React-Full/src/Day - 2/Props_Project.jsx
+++ React/React-Full/src/Day - 2/Props_Project.jsx
@@ -1,34 +1,30 @@
 import React from 'react'
 
-function Props_Project() {
-
-    let ago1 = "5 days ago"
-    let com1 = "Amazon "
+function Props_Project({ job }) {
 
     return (
-        <div className='parent'>
-            <div className="card">
-                <div className="top">
-                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTi6U-KSnY_r4hsTsVxDuaaM1suHkBJRHcyaf65xF5PJQ&s=10" alt="" />
-                    <button>Save</button>
-                </div>
-                <div className="center">
-                    <h3>{com1}<span>{ago1}</span></h3>
-                    <h2>Senior Backend Engineer</h2>
-                    <div className='tag'>
-                        <h4>Full Time</h4>
-                        <h4>Senior Level</h4>
-                    </div>
-                </div>
-                <div className="bottom">
-                    <div>
-                        <h3>$45k/Month</h3>
-                        <p>USA, San Fransisco</p>
-                    </div>
-                    <button>Apply Now</button>
+        <div className="card">
+            <div className="top">
+                <img src="{job.logo}" alt="" />
+                <button>Save</button>
+            </div>
+            <div className="center">
+                <h3> {job.company}<span> {job.posted}</span></h3>
+                <h2>{job.role}</h2>
+                <div className='tag'>
+                    <h4>{job.type}</h4>
+                    <h4>{job.level}</h4>
                 </div>
             </div>
+            <div className="bottom">
+                <div>
+                    <h3>{job.salary}</h3>
+                    <p>{job.location}</p>
+                </div>
+                <button>Apply Now</button>
+            </div>
         </div>
+
     )
 }
 
