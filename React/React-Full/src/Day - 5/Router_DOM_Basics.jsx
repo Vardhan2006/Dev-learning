ef2fbf7c924ccabb10f354925d27a4ad6b4c4f9c
Vardhan <ksaivardhan9@gmail.com>
2026-07-16 15:41:35 +0530

    Completed Basic Router DOM

--- React/React-Full/src/Day - 5/Router_DOM_Basics.jsx
+++ React/React-Full/src/Day - 5/Router_DOM_Basics.jsx
@@ -3,14 +3,18 @@ import { Route, Routes } from "react-router-dom";
 import Home from './Home';
 import About from './About';
 import Contact from './Contact';
+import Nav from "./Nav";
 
 function Router_DOM_Basics() {
   return (
     <div>
+      
+      <Nav/>
+
       <Routes>
-        <Route path='/home' element={<Home/>} />
-        <Route path='/about' element={<About/>} />
-        <Route path='/contact' element={<Contact/>} />
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/contact' element={<Contact />} />
       </Routes>
     </div>
   )
---
+++ React/React-Full/src/Day - 5/nav.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Nav() {
+    return (
+        <div className='nav'>
+            
+            <h3>Nav Task</h3>
+            
+            <a href="/">Home</a>
+            <a href="/about">About</a>
+            <a href="/contact">Contact</a>
+        
+        </div>
+    )
+}
+
+export default Nav
