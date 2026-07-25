723bbdb1002533c23125bb533f4f91dc6043664a
Vardhan <ksaivardhan9@gmail.com>
2026-07-25 16:28:32 +0530

    Crypto Module Completed

--- Backend/Node JS/src/02-crypto-module.ts
+++ Backend/Node JS/src/02-crypto-module.ts
@@ -3,7 +3,8 @@ related tasks like creating secure tokens, verifications like dats is changed or
 changed for encrypt and decrypt and hashing the data.
 */
 
-import crypto from "node:crypto";
+import crypto, { createHmac } from "node:crypto";
+import { create } from "node:domain";
 
 // crypto.randomUUID (universally unique identifier)
 
@@ -67,4 +68,57 @@ C:\Codes\Dev-learning\Backend\Node JS>npm run 02
 4e309c13891191cb6dc72b7f227d0e9a
 
 */
+
+
+
+
+
+// crypto.createHash (it will conevert the data into fixed length string)
+
+/*means, hashing is a one-way function, 
+meaning you cannot reverse a hash output back 
+into your original input.
+ ex: hello --> Hash (possible)
+     Hash --> hello (not possible)
+
+*/
+
+const text = "hellow world";
+const hash = crypto.createHash('SHA256').update('text').digest('hex');
+
+console.log(hash); // 982d9e3eb996f559e633f4d194def3761d909f5a3b647d1a851fead67c32c9d1
+
+
+
+// crypto.createHmac
+
+/*
+
+normal hash: data --> hash
+
+HMAC: data + secret --> signed hash
+
+common uses cases: webhook, signed tokens
+
+*/
+
+const secret = "my-secret-key"
+const message = "user_id = 01"
+
+const signature = crypto
+    .createHmac("SHA256", secret)
+    .update(message)
+    .digest('hex')
+
+console.log(signature); // da4b878bdd324f6533d8e7b9a436e51bc0af5bf966c00598a59594dd1de49c5d
+
+
+// verifying the signature
+
+const signatureVerify = crypto
+    .createHmac('SHA256', secret)
+    .update(message)
+    .digest('hex')
+
+console.log("Signature is valid and matching", signature === signatureVerify); // Signature is valid and matching true
 
